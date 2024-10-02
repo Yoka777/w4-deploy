7e71d0e815c654f4b888bb4901f59a8321485d78
Yoka777 <130054076+Yoka777@users.noreply.github.com>
2024-10-02 17:34:32 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   server: {
-    port: 80, 
+    port: 3000, 
     host: '0.0.0.0' 
   },
   compatibilityDate: '2024-04-03',
